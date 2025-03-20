024a17e5de5e9bb5f0742b45ada78b849dcac02d
Baptiste BERTRAND-RAPELLO <baptiste.bertrand-rapello@dedalus.com>
2025-03-20 17:37:20 +0100

    BBR: :memo: added teh readme (empty)

--- TestCLI/main.swift
+++ TestCLI/main.swift
@@ -9,3 +9,15 @@ import Foundation
 
 print("Hello, World!")
 
+
+let test : Int
+
+test = 42
+
+print("la valeur est de \(test)")
+
+let decimalInteger = 17
+let binaryInteger = 0b10001       // 17 in binary notation
+let octalInteger = 0o21           // 17 in octal notation
+let hexadecimalInteger = 0x11     // 17 in hexadecimal notation
+
